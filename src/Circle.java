e1d85586acf148a8a3773d5b1bf79aabd35d4d7f
Мээрим <paratova98>
2022-07-16 01:03:30 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private /* статик кайра жазылышы керек */ static final double PI=3.14;//өзгөрбөй турган свойства имитюбиль
+    public static double area(int radius){
+        return PI*(radius*radius); //1бизге берилген формула
+
+    }
+    public static double circumference(int radius ){
+        return PI*2*radius; //2
+    }
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Circle.area(14));
+        System.out.println(Circle.circumference(14));
+    }
+}
